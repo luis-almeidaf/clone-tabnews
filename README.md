4c20014b0b82065af3ebd34cd570048a1ce0895e
unknown <luis.almeida.faria@gmail.com>
2025-04-12 12:29:49 -0300

    add scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para aprender de fora a fora como criar um projeto do zero
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 function Home() {
-    return <h1>Vai Corinthians
-        
-    </h1>
+  return <h1>Vai Corinthians</h1>;
 }
 
 export default Home;
