4e1e38dba57c663c932e983fb4344d02ccd05d14
unknown <luis.almeida.faria@gmail.com>
2025-04-20 22:42:04 -0300

    Add a more robust way to handle other http request at `api/v1/migrations` endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -33,5 +33,9 @@ export default async function migrations(request, response) {
     return response.status(200).json(migratedMigrations);
   }
 
+  if (request.method !== "POST" || request.method !== "GET") {
+    return response.status(405).end();
+  }
+
   return response.status(405).end();
 }
