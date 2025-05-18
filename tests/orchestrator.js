8946367ee86b244abd8d3c530d8f7f90250113fa
unknown <luis.almeida.faria@gmail.com>
2025-05-18 20:28:17 -0300

    feat: create and use `orchestrator.createUser()`

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,6 +1,9 @@
+import retry from "async-retry";
+import { faker } from "@faker-js/faker";
+
 import database from "infra/database.js";
 import migrator from "models/migrator.js";
-import retry from "async-retry";
+import user from "models/user.js";
 
 async function waitForAllServices() {
   await waitForWebServer();
@@ -29,8 +32,18 @@ async function runPendingMigrations() {
   await migrator.runPendingMigrations();
 }
 
+async function createUser(userObject) {
+  return await user.create({
+    username:
+      userObject.username || faker.internet.username().replace(/[_.-]/g, ""),
+    email: userObject.email || faker.internet.email(),
+    password: userObject.password || "validpassword",
+  });
+}
+
 const orchestrator = {
   clearDatabase,
+  createUser,
   runPendingMigrations,
   waitForAllServices,
 };
